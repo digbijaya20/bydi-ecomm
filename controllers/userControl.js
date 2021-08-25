8a370066fa42a0b138fc6a236665cd730aead392
Digbijaya Biswal <digbijaya.biswal1234@gmail.com>
2021-08-25 16:29:57 +0530

    Added login data

--- controllers/userControl.js
+++ controllers/userControl.js
@@ -41,6 +41,41 @@ const userControl = {
            return res.status(500).json({msg: err.message})
        }
     },
+    login : async (req, res) =>{
+        try {
+            const  {email, password} = req.body;
+
+            const user= await Users.findOne({email});
+            if(!user) return res.status(400).json({msg: "User does not exist!!"})
+
+            const isMatch =await bcrypt.compare(password, user.password);
+            if(!isMatch) return res.status(400).json({msg: "Invalid password."})
+
+            // if login success, create access token and refresh token
+            const accesstoken = createAccessToken({id: user._id})
+            const refreshtoken = createRefreshToken({id: user._id})
+ 
+            res.cookie('refreshtoken', refreshtoken,{
+                httpOnly: true,
+                path: '/user/refresh_token',
+            })
+ 
+            res.json({accesstoken})
+
+            res.json({msg: "Login Success!"})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    logout: async (req, res) =>{
+        try {
+            res.clearCookie('refreshtoken', {path:'/user/refresh_token'})
+            return res.json({msg:"Logged out"})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+
+        }
+    },
     refreshToken: (req,res) =>{
         try{
             const rf_token = req.cookies.refreshtoken;
@@ -63,6 +98,16 @@ const userControl = {
 
         } catch(err){
             return res.status(500).json({msg: err.message})
+        }
+    },
+    getUser: async (req,res) =>{
+        try {
+            const user = await Users.findById(req.user.id).select('-password');
+            if(!user) return res.status(400).json({msg: "User does not exist."})
+            res.json(user)
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+
         }
     }
 
